d0ae806cdfa552334fd45d809b35d8d22cfefdfc
Nini Liao <u9513031@cmu.edu.tw>
2022-09-17 00:32:37 +0800

    setEmail、SetPwd

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import { useState, useContext, useEffect } from "react";
 import FirebaseContext from "../context/firebase";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 
@@ -29,10 +29,42 @@ export default function Login() {
         />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
-        </h1>  
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>  
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form>
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({target}) => setEmailAddress(target.value)}
+            />  
+            <input
+              aria-label="Enter your passwrod"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({target}) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${isInvalid && 'opacity-50'}`}
+            >Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+          <p className="text-sm">Don't have an account?{` `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
