00f921413785c9be295e425247ee1194bd69a444
Nini Liao <u9513031@cmu.edu.tw>
2022-09-16 23:10:54 +0800

    added firebase config

---
+++ src/lib/firebase.js
@@ -0,0 +1,25 @@
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// here i want to import seed file
+// import { seedDatabase } from '../seed';
+
+const config = {
+    apiKey: "AIzaSyBbF509QVe16WffzVmoblt3ql5075UxD9Q",
+    authDomain: "instagram-clone-with-rea-252d8.firebaseapp.com",
+    projectId: "instagram-clone-with-rea-252d8",
+    storageBucket: "instagram-clone-with-rea-252d8.appspot.com",
+    messagingSenderId: "310802979070",
+    appId: "1:310802979070:web:970228bc9e0b335f5aadaf"  
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+
+// here is where i want to call the seed file (only ONCE!)
+// seedDatabase(firebase);
+
+
+export { firebase, FieldValue };
