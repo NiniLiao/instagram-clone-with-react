693a1383c6ab0b6cec677ac62a93ec284c61d084
Nini Liao <u9513031@cmu.edu.tw>
2022-09-20 23:43:01 +0800

    added color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,27 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
-  // theme: {
-  //   fill: (theme) => ({
-  //     red: theme('colors.red.primary')
-  //   }),
-  //   colors: {
-  //     white: '#ffffff',
-  //     blue: {
-  //       medium: '#005c98'
-  //     },
-  //     block: {
-  //       light: '#005c98',
-  //       faded: '#00000059'
-  //     },
-  //     gray: {
-  //       base: '#616161',
-  //       background: '#fafafa',
-  //       primary: '#dbdbdb'
-  //     },
-  //     red: {
-  //       primary: '#ed4956'
-  //     }
-  //   }
-  // }
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      block: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
+  }
 };
