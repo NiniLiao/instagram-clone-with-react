5a07e6ac565e2bec5da6d9f74a26ad120b0541b2
Nini Liao <u9513031@cmu.edu.tw>
2022-09-27 20:53:42 +0800

    fixed: useState

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -1,29 +1,26 @@
-import { useStat, useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import UserContext from "../context/user";
 import { getPhotos, getUserByUserId } from "../services/firebase";
 
 export default function usePhotos() {
-  const [photos, setPhotos] = useStat(null);
+  const [photos, setPhotos] = useState(null);
   const {
     user: { uid: userId = '' }
   } = useContext(UserContext);
 
   useEffect(() => {
     async function getTimeLinePhotos() {
-      // example: [2, 1, 5] <- 2 being owell
+      // example: [2, 1, 5] <- 2 being raphel
       const [{ following }] = await getUserByUserId(userId);
       let followedUserPhotos = [];
 
-      console.log('following', following);
-
-      // does the user actually follow people?
+       // does the user actually follow people?
       if (following.length > 0) {
         followedUserPhotos = await getPhotos(userId, following);
       }
     }
 
-    console.log(userId);
-    // getTimeLinePhotos()
+    getTimeLinePhotos()
   }, []);
 
   return { photos };
