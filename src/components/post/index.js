15d8611ec5092a68aa870c611e2ba473a6621839
Nini Liao <u9513031@cmu.edu.tw>
2022-09-28 21:16:04 +0800

    specify the content proptypes

---
+++ src/components/post/index.js
@@ -0,0 +1,21 @@
+import { useRef } from "react";
+import PropTypes from 'prop-types';
+
+export default function Post({ content }) {
+  // components
+  //  -> header, image, actions (like & comment icons), footer, comments
+  return <p>I am a post</p>
+}
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired
+  })
+};
