fbe97f18accd71a94009c2bccac18938188dab12
Nini Liao <u9513031@cmu.edu.tw>
2022-10-17 20:24:32 +0800

    To complete the display of follower in header

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -13,8 +13,8 @@ export default function Header({
     docId: profileDocId,
     userId: profileUserId,
     fullName,
-    followers,
-    following,
+    followers = [],
+    following = [],
     username: profileUsername
   }
 }) {
@@ -22,7 +22,12 @@ export default function Header({
   const [isFollowingProfile, setIsFollowingProfile] = useState(false);
   const activeBtnFollow = user?.username && user?.username !== profileUsername;
 
-  const handleToggleFollow = () => 1;
+  const handleToggleFollow = () => {
+    setIsFollowingProfile((isFollowingProfile) => !isFollowingProfile);
+    setFollowerCount({
+      followerCount: isFollowingProfile ? followerCount - 1 : followerCount + 1
+    });
+  };
 
   useEffect(() => {
     const isLoggedInUserFollowingProfile = async() => {
@@ -63,6 +68,28 @@ export default function Header({
               {isFollowingProfile ? 'Unfollow' : 'Follow'}
             </button>
           )}
+        </div>
+        <div className="container flex mt-4">
+          {followers == undefined || following == undefined ? (
+            <Skeleton count={1} width={677} height={24} />
+          ) : (
+            <>
+              <p className="mr-10">
+                <span className="font-bold">{photosCount}</span> photos
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{followerCount}</span>
+                {' '}
+                {followerCount === 1 ? 'follower' : 'followers'}
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{following.length}</span> following
+              </p>
+            </>
+          )}
+        </div>
+        <div className="container mt-4">
+          <p className="font-medium">{!fullName ? <Skeleton count={1} height={24} /> : fullName}</p>
         </div>
       </div>
     </div>
