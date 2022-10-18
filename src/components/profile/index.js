87e8288300f296ca3532bccd8f1e78909c61be6c
Nini Liao <u9513031@cmu.edu.tw>
2022-10-18 21:19:38 +0800

    fixed: photoCollection

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -17,7 +17,7 @@ export default function Profile({ user }) {
   useEffect(() => {
     async function getProfileInfoAndPhotos() {
       const photos = await getUserPhotosByUsername(user.username);
-      dispatch({ profile: user, photosCollection: photos, followerCount: user.followers.length });
+      dispatch({ profile: user, photoCollection: photos, followerCount: user.followers.length });
     }
     getProfileInfoAndPhotos();
   }, [user.username]);
