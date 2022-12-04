46568f6f98a013b02b47204937e2d2ef511f32cb
Nini Liao <u9513031@cmu.edu.tw>
2022-12-04 17:57:11 +0800

    keep modifying protected route

--- src/helpers/protected-route.js
+++ src/helpers/protected-route.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 
 
 export default function ProtectedRoute({ user, children, ...rest }) {
   
     return (
-        <Route
-          {...rest}
-          render={({ location }) => {
-            if (user) {
-              return React.cloneElement(children, { user });
-            }
-    
-            if (!user) {
-              return (
-                <Navigate
-                  to={{
-                    pathname: ROUTES.LOGIN,
-                    state: { from: location }
-                  }}
-                />
-              );
-            }
-    
-            return null;
-          }}
-        />
-      );
-    }
-    
-    ProtectedRoute.propTypes = {
-      user: PropTypes.object,
-      children: PropTypes.object.isRequired
-    };
+        user ? children : <Navigate to='/login'/>
+      )
+}      
+
+ProtectedRoute.propTypes = {
+    user: PropTypes.object,
+    children: PropTypes.object.isRequired
+};
