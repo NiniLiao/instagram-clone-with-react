3eea81dabd78b6e4248a821a5dd930b9a0c64109
Nini Liao <u9513031@cmu.edu.tw>
2022-12-18 20:57:02 +0800

    added Routes

--- src/helpers/protected-route.js
+++ src/helpers/protected-route.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Navigate } from 'react-router-dom';
+import { Route, Navigate, Routes } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 
 
 export default function ProtectedRoute({ user, children, ...rest }) {
-  
     return (
-        user ? children : <Navigate to='/login'/>
-      )
-}      
+      <Routes>
+      <Route
+        {...rest}
+        render={({ location }) => {
+          if (user) {
+            return React.cloneElement(children, { user });
+          }
+  
+          if (!user) {
+            return (
+              <Navigate
+                to={{
+                  pathname: ROUTES.LOGIN,
+                  state: { from: location }
+                }}
+              />
+            );
+          }
+  
+          return null;
+        }}
+      />
+      </Routes>  
+    );
+  }    
 
 ProtectedRoute.propTypes = {
     user: PropTypes.object,
